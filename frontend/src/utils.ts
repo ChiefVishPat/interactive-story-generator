02e73327a65572a9c4a31554778dd1ff7552f641
Vishal Patel - personal <chiefvishpat@gmail.com>
2025-08-04 14:41:46 -0400

    configured utils.ts to the updated base url for choreo.

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = '/choreo-apis/create-your-own-adventure/backend/v1/api'
 
-// '/choreo-apis/choose-your-own-adventure/backend/v1/api';
+// replace the above with '/api' for local build. we can configure this later to dynamically choose the correct endpoint.
